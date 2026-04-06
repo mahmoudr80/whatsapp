dba9b104eadd17d44cc9d0a980cc9e98f57b6e00
Mahmoud <mahmoudsayeedr80@gmail.com>
2026-04-06 14:48:31 +0200

    ui of chat screen done✔

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp/core/theme/app_colors.dart';
+import 'package:whatsapp/feature/chat/presentation/chat_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body:
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("data",style: TextStyle(color: AppColors.seenTextColor),),
-      ],
-    ),),);
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white ,
+            primaryColor: Colors.white,
+          appBarTheme: AppBarTheme(backgroundColor: Colors.white,)
+        ),
+        home:
+        ChatScreen(),
+      ),
+    );
   }
 }
 
